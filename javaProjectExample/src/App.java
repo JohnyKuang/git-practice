7183144250ba5e0847b209557ae7656f4c66796f
Johny Kuang <kuang.johny2@gmail.com>
2021-02-18 14:36:02 -0800

    made changes to testFile

--- javaProjectExample/src/App.java
+++ javaProjectExample/src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        testFile myTestFile = new testFile(); 
-        System.out.println(myTestFile.saySomething());
+        // testFile myTestFile = new testFile;
+        int myNum = 2;
+        System.out.println(testFile.multiplyBy5(myNum));
 
     }
 }
--- javaProjectExample/src/testFile.java
+++ javaProjectExample/src/testFile.java
@@ -1,7 +1,15 @@
 public class testFile {
 
-    public String saySomething() {
+    public static String saySomething() {
         return "Ok, saying something!";
+    }
+
+    public static int multiplyBy5(int num) {
+        int result = 0 ;
+        for (int i = 0; i < 5; i++) {
+            result += num;
+        }
+        return result;
     }
 
 }
