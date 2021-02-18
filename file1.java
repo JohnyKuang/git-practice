c3863eafe2c991a2b28f1b3982a54c3db8b99c51
Johny Kuang <kuang.johny2@gmail.com>
2021-02-17 16:44:03 -0800

    changes to file1.java from comp2

--- file1.java
+++ file1.java
@@ -2,7 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
-        System.out.println("Here are some mods from branch file1Mods");
+        System.out.println("Here are some changes from computer 2");
+
     }
 
 }
