a72a634df2487b8a2f70d05bdb2e1d5bb85b5bc9
Johny Kuang <kuang.johny2@gmail.com>
2021-02-21 11:59:26 -0800

    made MyQueue.java which is queue made of stacks

---
+++ javaProjectExample/src/MyQueue.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+public class MyQueue {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if (stack1.empty() && stack2.empty()) {
+            return true;
+        }
+        return false;
+    }
+
+}
