37aab3f4ce58eb7713496a842211c9f60792e8d9
Johny Kuang <kuang.johny2@gmail.com>
2021-02-21 21:17:09 -0800

    did lc minDepth problem

--- javaProjectExample/src/App.java
+++ javaProjectExample/src/App.java
@@ -253,4 +253,21 @@ public class App {
 
     }
 
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        if (root.left != null && root.right != null) {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+        if (root.left != null) {
+            return minDepth(root.left) + 1;
+        } else {
+            return minDepth(root.right) + 1;
+        }
+    }
+
 }
