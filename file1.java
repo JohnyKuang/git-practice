7000108178759acc1cad59beb9031a6bfc98e601
Johny Kuang <kuajohny@amazon.com>
2021-03-11 18:29:39 -0800

    more changes to file1.java made on mac

--- file1.java
+++ file1.java
@@ -3,8 +3,8 @@ class HelloWorld {
         System.out.println("BRAND NEW MARCH 6");
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
-        System.out.println("Here are some mods from branch file1Mods");
-        System.out.println("Some more mods from file1Mods");
+        System.out.println("Some more changes made on mac");
+        System.out.println("Some more changes done on mac");
         System.out.println("Here are some changes made locally on mac");
     }
 
