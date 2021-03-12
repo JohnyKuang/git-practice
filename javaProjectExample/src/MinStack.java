8149d044f67ff23a871c9e503cd95745d1644eca
Johny Kuang <kuajohny@amazon.com>
2021-03-11 17:02:40 -0800

    added package names to top of FileReadWriteSample.java and MinStack.java

--- javaProjectExample/src/MinStack.java
+++ javaProjectExample/src/MinStack.java
@@ -1,3 +1,5 @@
+package javaProjectExample.src;
+
 import java.util.Stack;
 
 class MinStack {
@@ -33,8 +35,3 @@ class MinStack {
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such: MinStack obj =
- * new MinStack(); obj.push(x); obj.pop(); int param_3 = obj.top(); int param_4
- * = obj.getMin();
- */
