56fb07734591499618f8fb1722a378ec720e1c4f
Johny Kuang <kuajohny@amazon.com>
2021-03-11 18:19:34 -0800

    made a change in file.java on macbook

--- file1.java
+++ file1.java
@@ -5,7 +5,7 @@ class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Here are some mods from branch file1Mods");
         System.out.println("Some more mods from file1Mods");
-        System.out.println("Here are some changes made locally");
+        System.out.println("Here are some changes made locally on mac");
     }
 
 }
