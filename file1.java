3542213fbf76c60aca41a761444ed341c4e773d0
Johny Kuang <kuang.johny2@gmail.com>
2021-03-11 18:37:59 -0800

    made some changes to file1 locally on windows

--- file1.java
+++ file1.java
@@ -3,8 +3,8 @@ class HelloWorld {
         System.out.println("BRAND NEW MARCH 6");
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
-        System.out.println("Here are some mods from branch file1Mods");
-        System.out.println("Some more mods from file1Mods");
+        System.out.println("Here are some changes locally on windows");
+        System.out.println("Here are some changes locally on windows ");
         System.out.println("Here are some changes made locally on mac");
 
     }
